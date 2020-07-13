84ead35ccaabaa512e5440b08abceca85d7c7e1f
George Zhuang <xgz2@cornell.edu>
2020-07-13 16:31:37 -0400

    Initial Commit

---
+++ pic-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pic-ios
+//
+//  Created by George Zhuang on 7/13/20.
+//  Copyright © 2020 George, Jonna, Judy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
